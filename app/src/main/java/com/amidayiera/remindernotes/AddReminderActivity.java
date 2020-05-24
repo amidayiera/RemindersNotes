b29e51c84639f8bd5df2bb9e33435b22959bd7c6
amidayiera <amidayiera@gmail.com>
2020-05-24 15:05:10 +0300

    add reminder layout and java file

---
+++ app/src/main/java/com/amidayiera/remindernotes/AddReminderActivity.java
@@ -0,0 +1,14 @@
+package com.amidayiera.remindernotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_add_reminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_reminder);
+    }
+}
